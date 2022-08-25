8a6eb6a5efe3bc66bd9e5ad82d6ef34a9d2d67ba
Akankshi Priya <akankshipriya24@gmail.com>
2022-08-25 17:52:38 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Parent
+    {
+        public void print()
+        {
+            Console.WriteLine("Parent class");
+        }
+    }
+
+    class Child:Parent
+    {
+        public void print()
+        {
+            Console.WriteLine("Child class");//parent print method is overridden
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Child c = new Child();
+            c.print();
+            Console.ReadLine();
+        }
+    }
+}
